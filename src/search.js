5e58b49442b26bac2846694a2286ff5d7b302e14
guweimo <lyfw244885@hotmail.com>
2019-08-17 22:37:52 +0800

    解析 ES6 和 React JSX

--- src/search.js
+++ src/search.js
@@ -1,3 +1,16 @@
-import { helloworld } from './helloworld'
+'use strict'
 
-document.write(helloworld())
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Search extends React.Component {
+    render() {
+        return <div>Search Text</div>
+    }
+}
+
+
+ReactDOM.render(
+    <Search />,
+    document.getElementById('root')
+)
