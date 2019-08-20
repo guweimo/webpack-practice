f688c3b9082a462a2b7b2d2ef6db96b3a465e83d
guweimo <lyfw244885@hotmail.com>
2019-08-20 19:03:44 +0800

    代码分割和动态 import

--- src/search/index.js
+++ src/search/index.js
@@ -6,9 +6,30 @@ import './search.less'
 import logo from './images/logo.png'
 
 class Search extends React.Component {
+
+    constructor() {
+        super(...arguments)
+
+        this.state = {
+            Text: null
+        }
+    }
+
+    loadComponent() {
+        import('./text.js').then((Text) => {
+            this.setState({
+                Text: Text.default
+            })
+        })
+    }
+
     render() {
+        const { Text } = this.state
         return <div class="search-text">
-            漂亮 <img src={ logo } />
+            {
+                Text ? <Text /> : null
+            }
+            漂亮 <img src={ logo } onClick={ this.loadComponent.bind(this) } />
         </div>
     }
 }
